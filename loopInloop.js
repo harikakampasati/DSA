38ed87a2296be4206abf95d4ad8364af3999f15b
harikakampasati <harikaabotta@gmail.com>
2025-06-11 19:02:57 +0530

    loop in loop

---
+++ loopInloop.js
@@ -0,0 +1,48 @@
+//i<3, j<3 normal loop
+for(let i =0; i<3; i++){
+    for(let j= 0; j<3; j++){
+        console.log(i,"-->", j)
+    }
+}
+
+// i<5, j<i 
+for(let i =0; i<5; i++){
+    for(let j= 0; j<i; j++){
+        console.log(i,"-->", j)
+    }
+}
+
+// i<5, j<=i normal loop
+for(let i =0; i<5; i++){
+    for(let j= 0; j<=i; j++){
+        console.log(i,"-->", j)
+    }
+}
+
+//i<5, j>0 j reverse loop, i normal loop
+for(let i =0; i<5; i++){
+    for(let j= i; j>0; j--){
+        console.log(i,"-->", j)
+    }
+}
+
+//i<5 j>=0 j reverse loop, i normal loop
+for(let i =0; i<5; i++){
+    for(let j= i; j>=0; j--){
+        console.log(i,"-->", j)
+    }
+}
+
+// i>0 j<i i reverse loop j normal loop
+for(let i =5; i>0; i--){
+    for(let j= 0; j<i; j++){
+        console.log(i,"-->", j)
+    }
+}
+
+// i>0,j>0 both reverse loop
+for(let i =5; i>0; i--){
+    for(let j= i; j>0; j--){
+        console.log(i,"-->", j)
+    }
+}
